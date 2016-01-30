1dc2a8588c9d9d0c4188ceeca69bc087f99ba2b3
Dimitar Uzunov <uzunov-dimitar@users.noreply.github.com>
2016-01-30 13:14:09 +0100

    Dance Pad button definitions

--- TribalDancerGGJ/Assets/Scripts/BeatsEngine.cs
+++ TribalDancerGGJ/Assets/Scripts/BeatsEngine.cs
@@ -93,24 +93,18 @@ public class BeatsEngine : MonoBehaviour {
             }
         }
     }
-    //function
-    //1.check current score to determine difficulty
-    //2.generate notes sequence based on difficulty
 
-    //function
-    //3.light up a note with a switch statement
-
-    //function
-    //4.call 3. based on 2.
+    private void lightUpFloor(int beat) {
+        switch (beat) {
+            case 1: //light up btn 1;
+                break;
+            default: break;
+        }
+    }
 
     //function
     //5.bool to check if a beat has passed: numb-deltatime, if < 0 reset and true; light up edge of screen on exact beat
 
     //function
     //6.receive input - if within frame of ??? debug log state correct, else good, else miss; call 7.; use 5. maybe
-
-    //function
-    //7.calculate points
-
-
 }
