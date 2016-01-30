3ff923da8c82aa271a8650e015b4b2c86fecbd6e
Dimitar Uzunov <uzunov-dimitar@users.noreply.github.com>
2016-01-30 11:32:40 +0100

    Combo system implemented. Beat system began

--- TribalDancerGGJ/Assets/Scripts/BeatsEngine.cs
+++ TribalDancerGGJ/Assets/Scripts/BeatsEngine.cs
@@ -4,20 +4,95 @@ using System.Collections.Generic;
 
 public class BeatsEngine : MonoBehaviour {
 
-    private List<int> beatSequence = new List<int>();
+    [SerializeField] static int totalBeats = 4;
+    [SerializeField] List<int> comboMarks = new List<int>();
+    [SerializeField] List<int> ptsGoodGained = new List<int>();
+    [SerializeField] List<int> ptsPerfectGained = new List<int>();
 
-	void Start () {
-	    //call 2.
-	}
-	
-	void Update () {
-        //call 5. & 6.
-	}
+    private int currCombo = 0;
+    private int maxCombo = 0;
+    private int currComboMark = 0;
+    private int currPoints = 0;
 
-    int generateRand() {
-        return (Random.Range(0, 16)%8 + 1);
+    private int secondStep1 = 0;
+    private int secondStep2 = 0;
+
+    private int numbGameBtns = 8;
+
+    private int[] beatSequence1 = new int[totalBeats];
+    private int[] beatSequence2 = new int[totalBeats]; //if bS2[i] == bs2[i] set bS2[i] to 0; skip all bS2 that are 0
+
+    void Start() {
+        for (int i = 0; i < totalBeats; i++) { //initial sequence of 4 individual buttons
+            beatSequence1[i] = generateRand(numbGameBtns);
+        }
     }
 
+    void Update() {
+        //call 5. & 6.
+    }
+
+    private int generateRand(int numb) { //randomly chooses button   //maybe turn it into a coroutine?
+        return Random.Range(0, numb * 2) % numb;
+    }
+
+    private bool compareInput(int beat, int keyPressed) { //if keyPressed is the same as the beat numb return true, else false
+        return true;
+    }
+
+    private void checkHit(int newHit) { //check current combo
+        if (newHit == 0) {
+            if (currCombo > comboMarks[0]) {
+                //call combobreak
+                currComboMark = 0;
+            }
+            currCombo = 0;
+        } else {
+            currCombo += 1;
+            if (currCombo > maxCombo) maxCombo = currCombo;
+            if (newHit == 1) {
+                currPoints += ptsGoodGained[currComboMark];
+            } else if (newHit == 2) {
+                currPoints += ptsPerfectGained[currComboMark];
+            } else {
+                Debug.Log("Points calculation error");
+            }
+        }
+    }
+
+    private void generateInstruction() {
+        if (currComboMark > 0) {
+            secondStep1 = generateRand(totalBeats);
+            if (currComboMark == 2) {
+                do secondStep2 = generateRand(totalBeats);
+                while (secondStep2 == secondStep1);
+            }
+        }
+        for (int i = 0; i < totalBeats; i++) {
+            beatSequence1[i] = generateRand(numbGameBtns);
+            if (currComboMark == 1 && i == secondStep1) {
+                beatSequence2[i] = generateRand(numbGameBtns);
+                if (beatSequence1[i] == beatSequence2[i]) {
+                    beatSequence2[i] = numbGameBtns + 1;
+                }
+            } else if (currComboMark == 2 && (i == secondStep1 || i == secondStep2)) {
+                beatSequence2[i] = generateRand(numbGameBtns);
+                if (beatSequence1[i] == beatSequence2[i]) {
+                    beatSequence2[i] = numbGameBtns + 1;
+                }
+            } else if (currComboMark == 3 && i != secondStep1) {
+                beatSequence2[i] = generateRand(numbGameBtns);
+                if (beatSequence1[i] == beatSequence2[i]) {
+                    beatSequence2[i] = numbGameBtns + 1;
+                }
+            } else {
+                beatSequence2[i] = generateRand(numbGameBtns);
+                if (beatSequence1[i] == beatSequence2[i]) {
+                    beatSequence2[i] = numbGameBtns + 1;
+                }
+            }
+        }
+    }
     //function
     //1.check current score to determine difficulty
     //2.generate notes sequence based on difficulty
@@ -27,7 +102,7 @@ public class BeatsEngine : MonoBehaviour {
 
     //function
     //4.call 3. based on 2.
-    
+
     //function
     //5.bool to check if a beat has passed: numb-deltatime, if < 0 reset and true; light up edge of screen on exact beat
 
---
+++ TribalDancerGGJ/Assets/Scripts/GameManagement.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class GameManagement : MonoBehaviour {
+
+    public static GameManagement instance;
+    void Awake() {
+        if (instance == null) {
+            instance = this;
+        } else if (instance != this) {
+            Destroy(gameObject);
+        }
+    }
+    public void LoadScene(string name) {
+        SceneManager.LoadScene(name);
+    }
+    public void QuitGame() {
+        Application.Quit();
+    }
+}
